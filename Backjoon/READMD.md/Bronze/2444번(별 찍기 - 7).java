24f18a231decfc358ee75e7cfd3709ad0e84e47f
0taeho0 <166297377+0taeho0@users.noreply.github.com>
2024-07-22 01:07:41 +0900

    Create 2444번(별 찍기 - 7).java

---
+++ Backjoon/READMD.md/Bronze/2444번(별 찍기 - 7).java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    	Scanner scan = new Scanner(System.in);
+    	
+    	int A = scan.nextInt();
+    	int count = 1;
+    	
+    	for(int i=0; i<A; i++) {
+    		for(int z=A-1; z>i; z--) {
+    			System.out.print(" ");
+    		}
+    		for(int j=1; j<=(count * 2)-1; j++) {
+    			System.out.print("*");
+    		}
+    		System.out.println();
+    		count++;
+    	}
+    	count = 3;
+    	for(int i=0; i<A-1; i++) {
+    		for(int z=0; z<i+1; z++) {
+    			System.out.print(" ");
+    		}
+    		for(int j=1; j<=(A * 2)-count; j++) {
+    			System.out.print("*");
+    		}
+    		System.out.println();
+    		count+=2;
+    	}
+    }
+}
