046f29717d21d29fb21c8ceb27d70f689e3aa2e8
0taeho0 <166297377+0taeho0@users.noreply.github.com>
2024-07-22 00:22:46 +0900

    Create 20492번(세금).java

---
+++ Backjoon/READMD.md/Bronze/20492번(세금).java
@@ -0,0 +1,26 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    	Scanner scan = new Scanner(System.in);
+    	
+    	long A = scan.nextInt();
+    	long B = Math.round(A * 0.8)+Math.round(Math.round(A * 0.2) * 0.78);
+    	System.out.println(Math.round(A * 0.78)
+    			+" "+ B);
+    }
+}
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    	Scanner scan = new Scanner(System.in);
+    	
+    	long A = scan.nextInt();
+    	long B = Math.round(A * 0.8)+Math.round(Math.round(A * 0.2) * 0.78);
+    	System.out.println(Math.round(A * 0.78)
+    			+" "+ B);
+    }
+}
